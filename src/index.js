0cd871adb09acb2f009e48a3a8e5da29b8de9402
AsuraMzn <ribubaucha@gmail.com>
2024-12-22 14:32:50 +0545

    Added interaction for user join and leave

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { log } from "console";
+import { channel } from "diagnostics_channel";
+import { Client, Events, GatewayIntentBits, GuildChannel } from "discord.js";
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
   ],
@@ -20,11 +23,44 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 });
 
+client.on(Events.GuildMemberAdd, (member) => {
+  const CHANNEL_ID= '1079103180853497967';
+  console.log(`New Member Joined! ${member}`);
+  const channel = client.channels.cache.get(CHANNEL_ID);
+  if (channel) {
+    channel.send(`New Member Joined! ${member}`);
+    member.send("Welcome To Our Server!");
+  } else {
+    console.log("Channel not found");
+  }
+})
+
+client.on(Events.GuildMemberRemove, async (member) => {
+  const CHANNEL_ID= '1079103180853497967';
+  console.log(`Member Left! ${member}`);
+  const channel = client.channels.cache.get(CHANNEL_ID);
+  if (channel) {
+    channel.send(`Member Left! ${member}`);
+    try {
+      await member.send(`Byeeee!`);
+      console.log(`Sent goodbye message to ${member.displayName}`);
+    } catch (error) {
+        if (error.code === 50007) {
+            console.error(`Cannot send messages to ${member.displayName}:`, error.message);
+        } else {
+            console.error(`Failed to send message to ${member.displayName}:`, error);
+        }
+    }
+  } else {
+    console.log("Channel not found");
+  }
+})
+
 client.on(Events.MessageCreate, (message) => {
   // console.log(message.content);
   if (message.author.bot) return;
   message.reply({
-    content: `Hey ${message.author.username}`,
+    content: `Hey ${message}`,
   });
 });
 
