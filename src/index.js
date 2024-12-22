c10b79869d9922d92ac440f414f8192a50b19689
AsuraMzn <ribubaucha@gmail.com>
2024-12-22 12:40:28 +0545

    Bot Setup

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on(Events.ClientReady, (readyClient) => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.on(Events.MessageCreate, (message) => {
+  // console.log(message.content);
+  if (message.author.bot) return;
+  message.reply({
+    content: `Hey ${message.author.username}`,
+  });
+});
+
+client.login(process.env.TOKEN);
